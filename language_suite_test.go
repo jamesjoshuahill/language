6fae53d806e6b1e4a060fedf8959d0d40c2ef691
Josh Hill <jamesjoshuahill@gmail.com>
2017-11-26 09:37:23 +0000

    Create server binary

---
+++ language_suite_test.go
@@ -0,0 +1,26 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+
+	"testing"
+)
+
+var serverBinaryPath string
+
+func TestLanguage(t *testing.T) {
+	BeforeSuite(func() {
+		var err error
+		serverBinaryPath, err = gexec.Build("github.com/jamesjoshuahill/language")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterSuite(func() {
+		gexec.CleanupBuildArtifacts()
+	})
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Language Suite")
+}
---
+++ server_test.go
@@ -0,0 +1,19 @@
+package main_test
+
+import (
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Server", func() {
+	It("compiles", func() {
+		cmd := exec.Command(serverBinaryPath)
+		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session).Should(gexec.Exit(0))
+	})
+})

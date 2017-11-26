8d0f9b4158a998fc1c15cb94cd797b819954c2f9
Josh Hill <jamesjoshuahill@gmail.com>
2017-11-26 10:11:13 +0000

    Listen on port 5555

--- server.go
+++ server.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := 5555
+	fmt.Printf("Starting server on port %d...", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
--- server_test.go
+++ server_test.go
@@ -1,19 +1,51 @@
 package main_test
 
 import (
+	"fmt"
+	"net"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 
 	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Server", func() {
-	It("compiles", func() {
-		cmd := exec.Command(serverBinaryPath)
-		session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(session).Should(gexec.Exit(0))
+	var session *gexec.Session
+
+	BeforeEach(func() {
+		session = startServer(5555)
+	})
+
+	AfterEach(func() {
+		gexec.KillAndWait()
+	})
+
+	It("listens on port 5555", func() {
+		Expect(session.Out).To(gbytes.Say("Starting server on port 5555..."))
+		Consistently(session).ShouldNot(gexec.Exit())
 	})
 })
+
+func startServer(port int) *gexec.Session {
+	cmd := exec.Command(serverBinaryPath)
+	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+
+	Eventually(listeningOn(port)).Should(BeTrue())
+	return session
+}
+
+func listeningOn(port int) func() bool {
+	return func() bool {
+		conn, err := net.Dial("tcp", fmt.Sprintf(":%d", port))
+		if err != nil {
+			return false
+		}
+
+		conn.Close()
+		return true
+	}
+}

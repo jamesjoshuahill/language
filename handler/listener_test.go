dc830866f7ff7f14c7a940a8c5d62b4898a213f0
Josh Hill <jamesjoshuahill@gmail.com>
2017-12-05 18:13:40 +0000

    Test that recorder receives word read from connection

--- handler/listener_test.go
+++ handler/listener_test.go
@@ -1,7 +1,7 @@
 package handler_test
 
 import (
-	"io"
+	"bytes"
 
 	"github.com/jamesjoshuahill/language/handler"
 	"github.com/jamesjoshuahill/language/handler/fakes"
@@ -14,11 +14,14 @@ var _ = Describe("Handler", func() {
 	It("has a language handler", func() {
 		recorder := new(fakes.FakeRecorder)
 		listener := handler.New(recorder)
+		reader := bytes.NewReader([]byte("hello"))
 		conn := new(fakes.FakeConn)
-		conn.ReadReturns(0, io.EOF)
+		conn.ReadStub = reader.Read
 
 		listener.Handle(conn)
 
-		Expect(conn.ReadCallCount()).To(Equal(1))
+		Expect(conn.ReadCallCount()).To(Equal(2))
+		Expect(recorder.RecordCallCount()).To(Equal(1))
+		Expect(recorder.RecordArgsForCall(0)).To(Equal("hello"))
 	})
 })
